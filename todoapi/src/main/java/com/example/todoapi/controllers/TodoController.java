7b7fb9f1bcdd1849b3d7275cc53394dfe7834f7b
geoff4fsm <87156765+geoff4fsm@users.noreply.github.com>
2021-11-05 15:25:32 -0400

    Add files via upload

---
+++ todoapi/src/main/java/com/example/todoapi/controllers/TodoController.java
@@ -0,0 +1,36 @@
+package com.example.todoapi.controllers;
+import java.util.*;
+import com.example.todoapi.entities.Todo;
+import com.example.todoapi.entities.TodoRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private final TodoRepository todoRepository;
+
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @GetMapping
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+
+    @GetMapping("/{todoId}")
+    public Optional<Todo> getTodo(@PathVariable("todoId") Long todoId) {
+        var todo = todoRepository.findById(todoId);
+        return todo;
+    }
+    @PostMapping
+    public Todo newTodo(@RequestBody Todo todo) {
+       return this.todoRepository.save(todo);
+    }
+    @PutMapping("/{todoId}")
+    public Optional<Todo> updateTodo(@PathVariable("todoId") Long todoId, @RequestBody Todo updatedTodo) {
+        return this.todoRepository.findById(todoId)
+                .map(oldTodo -> this.todoRepository.save(updatedTodo));
+    }
+}
